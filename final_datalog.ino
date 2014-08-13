7640f09bde3498fb0b64aea37920ca972438ecac
Naveen <naveen246@gmail.com>
2014-08-13 09:28:39 +0530

    2 min fast log taking 6 min wen teted with url http://169.254.101.102/?log=f# on browser

--- final_datalog.ino
+++ final_datalog.ino
@@ -13,18 +13,36 @@ EthernetServer server(80);  // create a server at port 80
 byte tick = 0;
 
 byte buf[DATA_BUF_SIZE];
+byte logBuf[DATA_BUF_SIZE];
+
+int logBufIndex = 0;
 
 File dataFile, logFile;
 
+String dataBuf = "";
+int lCount = 0, fCount = 1;
+
 const int maxLineCount = 30000;
+const int maxFileCount = 8;
 
-boolean canDownload = false;
+byte canDownload[maxFileCount] = { 0 };
 
-unsigned short getDigitalData() {
-    unsigned short data = 0, val;  
-    for(int pin = 22; pin <= 37; pin++) {
+EthernetClient client;
+
+byte getHiByte() {
+    byte data = 0, val;
+    for(int pin = 22; pin <= 29; pin++) {
         val = digitalRead(pin);
         data |= val << (pin - 22);
+    }
+    return data;
+}
+
+byte getLowByte() {
+    byte data = 0, val;
+    for(int pin = 30; pin <= 37; pin++) {
+        val = digitalRead(pin);
+        data |= val << (pin - 30);
     }
     return data;
 }
@@ -38,32 +56,73 @@ int tempDelay() {
     return data;
 }
 
-void logData() {
-    static int lineCount = 0;
-    if(lineCount < maxLineCount) {
-        lineCount++;
-        //char data[13] = { '\0' };
-        String data = "";
-        String temp = "";
-        int dataIndex = 0;
-        for(int i = 0; i < 4; i++) {
-            unsigned short digitalData = getDigitalData(); 
-            data = data + digitalData;
-            if(i < 3)
-              data += ',';
+
+void logFastData() {
+    char fileName[9] = { "log1.txt" };
+    byte hiByte, loByte;
+    if(lCount == 0) {
+        if(fCount == 1) {
+            logBufIndex = 0; 
         }
-        
-        //the following loop is for delay
-        for(int i = 0; i < 8; i++) {
-            temp = String(data) + "\t" + tempDelay(); 
-        }
-        logFile.println(data);
-    } else {
-        logFile.close();
-        Serial.println("closing logFile");
-        Timer3.detachInterrupt();
-        canDownload = true; 
+        fileName[3] = fCount + 48;
+        logFile = SD.open(fileName, FILE_WRITE);
     }
+    
+    if(lCount < maxLineCount) {
+        lCount++;
+        String temp = "";
+        for(int i = 0; i < 4; i++) {
+            logBuf[logBufIndex] = getHiByte();
+            logBufIndex++;
+            logBuf[logBufIndex] = getLowByte();
+            logBufIndex++;
+            //delay code follows
+            for(int i = 0; i < 8; i++) {
+                temp = String(hiByte) + "\t" + tempDelay(); 
+            }
+            //logFile.write(logBuf[logBufIndex]);
+            //logFile.write(logBuf[logBufIndex+1]);
+        }
+        if(logBufIndex >= DATA_BUF_SIZE || lCount >= maxLineCount) {
+            logFile.write(logBuf, logBufIndex);
+            logBufIndex = 0;
+        }
+    }else {
+        logFile.close();
+        canDownload[fCount - 1] = 1;
+        lCount = 0;
+        fCount++;
+        if(fCount > maxFileCount) {
+            Timer3.detachInterrupt();
+            Serial.println("detaching interrupt ");
+        }
+        Serial.println("closing logFile ");
+    }
+}
+
+void sendFastLog() {
+    char fileName[9] = { "log1.txt" };
+    for(byte fileCount = 1; fileCount <= maxFileCount; fileCount++) {
+        while(canDownload[fileCount - 1] == 0){
+           Serial.println("cannot download " + String(fileCount));
+           delay(2000);
+        }   
+        Serial.println("can download " + String(fileCount));
+        fileName[3] = fileCount + 48;
+        dataFile = SD.open(fileName);     
+        if (dataFile) {
+            int bytesAvailable = 0;
+            while(bytesAvailable = dataFile.available()) {
+                int i;
+                for(i = 0; i < DATA_BUF_SIZE && i < bytesAvailable; i++) {
+                    buf[i] = dataFile.read();
+                }
+                client.write(buf, i);
+                //client.write(dataFile.read()); 
+            }
+            dataFile.close();
+        }
+    } 
 }
 
 void setup()
@@ -83,63 +142,54 @@ void setup()
     }
     Serial.println("SUCCESS - SD card initialized.");
     SD.remove("log1.txt");
-    delay(500);
-    Serial.println("creating log file");
-    logFile = SD.open("log1.txt", FILE_WRITE);
+    SD.remove("log2.txt");
+    SD.remove("log3.txt");
+    SD.remove("log4.txt");
+    SD.remove("log5.txt");
+    SD.remove("log6.txt");
+    SD.remove("log7.txt");
+    SD.remove("log8.txt");
+    delay(1000);
     
     for(int pin = 22; pin <= 37; pin++) {
         pinMode(pin, INPUT);      
     }
     delay(1000);
-    Serial.println("calling interrupt");
-    Timer3.attachInterrupt(logData).start(500);
-    delay(1000);
+    
 }
 
 void loop()
 {
-    EthernetClient client = server.available();  // try to get client
+    client = server.available();  // try to get client
 
     if (client) {  // got client?
         boolean currentLineIsBlank = true;
         while (client.connected()) {
             if (client.available()) {   // client data available to read
-                char c = client.read(); // read 1 byte (character) from client
-                // last line of client request is blank and ends with \n
-                // respond to client only after last line received
-                if (c == '\n' && currentLineIsBlank && canDownload) {
-                    // send a standard http response header
+                if( client.find("GET ") ) {
+                    String request = client.readStringUntil('#');
+                    Serial.println(request);
+                    //while(client.findUntil("log", "\n\r"))
+                        //char logType = client.read(); // f or n
+                
                     client.println("HTTP/1.1 200 OK");
                     client.println("Content-type: text/html");
-                    //client.println("Content-disposition: attachment; filename=log1.txt");
+                    client.println("Content-disposition: attachment; filename=log1.txt");
                     client.println("Connection: close");
                     client.println();
-                    // send web page
-                    dataFile = SD.open("log1.txt");        
-                    if (dataFile) {
-                        int bytesAvailable = 0;
-                        while(bytesAvailable = dataFile.available()) {
-                            int i;
-                            for(i = 0; i < DATA_BUF_SIZE && i < bytesAvailable; i++) {
-                                buf[i] = dataFile.read();
-                            }
-                            client.write(buf, i);
-                            //client.write(dataFile.read()); 
-                            c = client.read(); // to read commands while sending file
+                    
+                    //send data
+                    if(request.indexOf("log=f") != -1) {
+                        lCount = 0; 
+                        fCount = 1;
+                        for(int i = 0; i < maxFileCount; i++) {
+                            canDownload[i] = 0;  
                         }
-                        dataFile.close();
+                        Timer3.attachInterrupt(logFastData).start(500);
+                        delay(1000);
+                        sendFastLog();  
                     }
                     break;
-                }
-                // every line of text received from the client ends with \r\n
-                if (c == '\n') {
-                    // last character on line of received text
-                    // starting new line with next character read
-                    currentLineIsBlank = true;
-                } 
-                else if (c != '\r') {
-                    // a text character was received from client
-                    currentLineIsBlank = false;
                 }
             } 
         } 
